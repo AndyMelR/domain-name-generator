ff92e0ca38b6986f32f11a209c2bb9f4a49b382d
AndyMelR <ameleror@hotmail.es>
2023-02-19 06:16:59 +0000

    generating domain

--- src/app.js
+++ src/app.js
@@ -7,10 +7,44 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log(domainNames);
 };
 
-var pronoun = ["the", "our"];
-var adj = ["great", "big"];
-var noun = ["jogger", "racoon"];
-var domain;
+const pronouns = ["the", "our"];
+const adjectives = ["great", "big"];
+const nouns = ["jogger", "racoon"];
+const extensions = [".com", ".net", ".us", ".io"];
+const domainHacks = [".us"];
+
+const domainNames = [];
+
+pronouns.forEach(pronoun => {
+  adjectives.forEach(adj => {
+    nouns.forEach(noun => {
+      const domainName = `${pronoun}${adj}${noun}`;
+
+      // Add different extensions
+      extensions.forEach(ext => {
+        domainNames.push(`${domainName}${ext}`);
+      });
+
+      // Add domain hacks
+      domainHacks.forEach(hack => {
+        const domainHackName = domainName.replace(/o/g, "");
+        domainNames.push(`${domainHackName}${hack}`);
+      });
+    });
+  });
+});
+
+const generateBtn = document.getElementById("generate-btn");
+const domainList = document.getElementById("domain-list");
+
+generateBtn.addEventListener("click", () => {
+  domainList.innerHTML = "";
+  domainNames.forEach(domain => {
+    const listItem = document.createElement("li");
+    listItem.textContent = domain;
+    domainList.appendChild(listItem);
+  });
+});
