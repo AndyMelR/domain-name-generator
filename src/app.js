d8bd4bda29b1faea637b8f68c2f53788a7d0936f
AndyMelR <ameleror@hotmail.es>
2023-02-21 19:14:12 +0000

    refactor to foreach

--- src/app.js
+++ src/app.js
@@ -4,68 +4,36 @@ import "./style.css";
 
 executeProgram();
 
-function executeProgram() {
-  window.onload = () => {
-    //write your code here
-    document.querySelector("#generate-btn").addEventListener("click", () => {
-      const domainName = getDomainName();
-      addExtension(domainName);
-      document.querySelector("#listOfDomains").innerHTML = showListOfDomains(5);
-      document.querySelector("#listOfDomains").innerHTML = domainNames.join(
-        "<br>"
-      );
-    });
-  };
-}
-
 const pronouns = ["the", "our", "my"];
 const adjectives = ["great", "big", "wow", "best"];
 const nouns = ["jogger", "racoon", "company", "world"];
 const extensions = [".com", ".net", ".us", ".io"];
-const domainNames = [];
-
-const getPronoun = pronouns => {
-  return pronouns[Math.floor(Math.random() * pronouns.length)];
-};
-
-const getAdjective = adjecives => {
-  return adjecives[Math.floor(Math.random() * adjecives.length)];
-};
-
-const getNoun = nouns => {
-  return nouns[Math.floor(Math.random() * nouns.length)];
-};
-
-const constructDomainName = (
-  selectedPronoun,
-  selectedAdjective,
-  selectedNoun
-) => {
-  return `${selectedPronoun}${selectedAdjective}${selectedNoun}`;
-};
-
-const addExtension = domainName => {
-  extensions.forEach(ext => {
-    domainNames.push(`${domainName}${ext}`);
-  });
-};
 
 const getDomainName = () => {
-  const selectedPronoun = getPronoun(pronouns);
-  const selectedAdjective = getAdjective(adjectives);
-  const selectedNoun = getNoun(nouns);
-  const domainName = constructDomainName(
-    selectedPronoun,
-    selectedAdjective,
-    selectedNoun
-  );
-  return domainName;
+  const domainNameParts = [];
+
+  pronouns.forEach(pronoun => {
+    adjectives.forEach(adjective => {
+      nouns.forEach(noun => {
+        extensions.forEach(extension => {
+          domainNameParts.push(`${pronoun}${adjective}${noun}${extension}`);
+        });
+      });
+    });
+  });
+
+  return domainNameParts;
 };
 
-const showListOfDomains = (maxCount = domainNames.length) => {
-  const listItems = domainNames
-    .slice(0, maxCount)
-    .map((domainName, index) => `<li>${index + 1}. ${domainName}</li>`);
-  const list = `<ul>${listItems.join("")}</ul>`;
-  return list;
-};
+const listOfDomains = getDomainName()
+  .map(domain => `<p>${domain}</p>`)
+  .join("");
+
+function executeProgram() {
+  window.onload = function() {
+    const generateButton = document.querySelector("#generate-btn");
+    generateButton.addEventListener("click", () => {
+      document.querySelector("#listOfDomains").innerHTML = listOfDomains;
+    });
+  };
+}
